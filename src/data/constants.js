2c55d811cfafe41e38db550d3b0de1cbb1d74e63
anurag721721 <anurag.yadav@aayaninfotech.com>
2025-07-24 13:55:17 +0530

    added loader and login issue fix

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL="http://34.230.196.168:3210/api"
-export const API_BASE_URL="http://localhost:3210/api"
+export const API_BASE_URL="http://34.230.196.168:3210/api"
+// export const API_BASE_URL="http://localhost:3210/api"
